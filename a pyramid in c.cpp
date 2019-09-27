672892b4ba7b647e5dacd828d44cf57d4d4925b7
saidutth <54472374+saidutth@users.noreply.github.com>
2019-09-27 17:33:38 +0530

    Add files via upload

---
+++ a pyramid in c.cpp
@@ -0,0 +1,32 @@
+/** writing a program to print the given pyramid
+12345
+
+1234
+
+123
+
+12
+
+1
+
+
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=1,j;
+	for (j=5;j<=1;--j)
+	{  
+	   
+		while(i<=j)
+		{
+			printf("%d",i);
+			i++;
+	}
+	printf("\n");
+	}
+	
+return 0;
+getch;	
+ } 
